4cb68bcb218577cbd7dd7319b857ecf612832ef6
Michael Bruce <me@michaelbruce.co>
2019-09-29 21:56:58 +0100

    now we are picking up default config automatically

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,2 @@
-((clojure-mode . ((cider-default-cljs-repl . figwheel-main)
-		  (cider-figwheel-main-default-options . ":dev"))))
+((nil . ((cider-default-cljs-repl . figwheel-main)
+	 (cider-figwheel-main-default-options . "nutrack"))))
