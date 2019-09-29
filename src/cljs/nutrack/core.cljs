504588ebca0871bb050a96af9d6b35107f84fc62
Michael Bruce <me@michaelbruce.co>
2019-09-29 17:26:53 +0100

    also swap icon on expand

--- src/cljs/nutrack/core.cljs
+++ src/cljs/nutrack/core.cljs
@@ -18,6 +18,12 @@
   {:d
      "M143 352.3L7 216.3c-9.4-9.4-9.4-24.6 0-33.9l22.6-22.6c9.4-9.4 24.6-9.4 33.9 0l96.4 96.4 96.4-96.4c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9l-136 136c-9.2 9.4-24.4 9.4-33.8 0z"}]])
 
+(def angle-up
+  [:svg {:viewBox "0 0 320 512", :xmlns "http://www.w3.org/2000/svg"}
+ [:path
+  {:d
+     "M177 159.7l136 136c9.4 9.4 9.4 24.6 0 33.9l-22.6 22.6c-9.4 9.4-24.6 9.4-33.9 0L160 255.9l-96.4 96.4c-9.4 9.4-24.6 9.4-33.9 0L7 329.7c-9.4-9.4-9.4-24.6 0-33.9l136-136c9.4-9.5 24.6-9.5 34-.1z"}]])
+
 (def ingredients
   ["Steak"
    "Rice"
@@ -47,7 +53,7 @@
       (let [child-height (:child-height @s)
             open? (:open @s)]
         [:section.expandable {:on-click #(swap! s update :open not)}
-         [:div title angle-down]
+         [:div title (if open? angle-up angle-down)]
          [:div {:style {:max-height (if open? child-height 0)
                         :transition "max-height 0.8s"
                         :overflow "hidden"}}
