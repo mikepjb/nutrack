5934c830bacdeefa9abd0f0bf97d678e30f40f71
Michael Bruce <me@michaelbruce.co>
2019-09-29 19:03:37 +0100

    store ingredients in re-frame db & clear up react warnings

--- src/cljs/nutrack/core.cljs
+++ src/cljs/nutrack/core.cljs
@@ -48,10 +48,23 @@
              :on-change #(reset! input (-> % .-target .-value))}]
     [suggestions @input]]])
 
+(def ingredient-table
+  [["Name" "Protein" "Carbs" "Fat" "Amount" "Cost"]
+   ["Chicken","24g","0g","8g","2000g","£22.24"]
+   ["Beef Mince","19.7g","0g","14.9g","900g","£4.50"]
+   ["Blueberries","0.7g","14.5g","0.3g","150g","£2.00"]
+   ["Oat Milk","1g","6.6g","2.8g","1000g","£1.80"]
+   ["Whey","90g","2.5g","0.3g","125g","£1.75"]
+   ["Egg","12.6g","0.4g","9g","400g","£1.60"]
+   ["Chopped Tomatoes","1.3g","3.6g","0.2g","800g","£1.00"]
+   ["Egg Noodles","4.7g","25.7g","0.5g","250g","£0.99"]
+   ["Butter","0.6g","0.7g","82g","100g","£0.80"]])
+
 (rf/reg-event-db
  :initialize
  (fn [_ _]
-   {}))
+   {:tables {:ingredients {:header (first ingredient-table)
+                           :rows (vec (rest ingredient-table))}}}))
 
 (rf/reg-event-db
  :panel/toggle
@@ -62,6 +75,11 @@
  :panel/state
  (fn [db [_ id]]
    (get-in db [:panels id])))
+
+(rf/reg-sub
+ ::tables
+ (fn [db [_ key]]
+   (get-in db [:tables key])))
 
 (defn expandable-component [id title]
   (let [s (reagent/atom {})] ;; will hold the child-height still..
@@ -78,28 +96,21 @@
                     (swap! s assoc :child-height (.-clientHeight %)))}
            "This is a recipe to create Tikka Masala"]]]))))
 
-(def ingredient-table
-  [["Name" "Protein" "Carbs" "Fat" "Amount" "Cost"]
-   ["Chicken","24g","0g","8g","2000g","£22.24"]
-   ["Beef Mince","19.7g","0g","14.9g","900g","£4.50"]
-   ["Blueberries","0.7g","14.5g","0.3g","150g","£2.00"]
-   ["Oat Milk","1g","6.6g","2.8g","1000g","£1.80"]
-   ["Whey","90g","2.5g","0.3g","125g","£1.75"]
-   ["Egg","12.6g","0.4g","9g","400g","£1.60"]
-   ["Chopped Tomatoes","1.3g","3.6g","0.2g","800g","£1.00"]
-   ["Egg Noodles","4.7g","25.7g","0.5g","250g","£0.99"]
-   ["Butter","0.6g","0.7g","82g","100g","£0.80"]])
-
 (defn table [data]
   [:section.table
-   [:table
-    [:tr
-     (for [h (first data)]
-       [:th h])]
-    (for [row (rest data)]
-      [:tr
-       (for [v row]
-         [:td v])])]])
+   (let [table @(rf/subscribe [::tables :ingredients])]
+     [:table
+      ^{:key :header}
+      [:tbody
+       [:tr
+        (for [h (:header table)]
+          ^{:key h} [:th h])]
+       (for [row (:rows table)]
+         ^{:key (first row)}
+         [:tr
+          (for [v row]
+            ^{:key (str (first row) "-" v)}
+            [:td v])])]])])
 
 (defn page [input]
   [:div.background
@@ -108,4 +119,5 @@
    [table ingredient-table]
    [expandable-component :tikka-id "Tikka Masala Recipe"]])
 
+(defonce _init (rf/dispatch-sync [:initialize]))
 (reagent/render [page input] (.getElementById js/document "app"))
